5553c897cee280299d7dd69c7b45d4f7b56b5eff
Ivan Sz <68451124+Ivanszs@users.noreply.github.com>
2021-02-03 13:56:11 -0300

    Add files via upload

---
+++ mm-sliding-puzzle/4x4/app.js
@@ -0,0 +1,86 @@
+const tiles = Array.from(document.querySelectorAll(".tile"));
+const emptyTile = document.querySelector(".tile--empty");
+const footer = document.querySelector(".footer");
+
+//set the interactions, for example: if blank tile is in A, then only B and D can be accessed
+const charInteractions = {
+  A: ["B", "E"],
+	B: ["A", "F", "C"],
+	C: ["B", "G", "D"],
+	D: ["C", "H"],
+	E: ["A", "F", "I"],
+	F: ["B", "E", "G", "J"],
+	G: ["C", "F", "H", "K"],
+	H: ["D", "G", "L"],
+  I: ["E", "J", "M"],
+  J: ["F", "I", "K", "N"],
+  K: ["G", "J", "L", "O"],
+  L: ["H", "K", "P"],
+  M: ["I", "N"],
+  N: ["J", "M", "O"],
+  O: ["K", "N", "P"],
+  P: ["L", "O"]
+};
+
+//lock/unlock tiles with .disabled
+const unlockTiles = currentTileArea => {
+  tiles.map(tile => {
+    const tileArea = tile.style.getPropertyValue("--char");
+    if (charInteractions[currentTileArea].includes(tileArea)) {
+      tile.disabled = false;
+    } else {
+      tile.disabled = true;
+    }
+  });
+  isComplete(tiles);
+};
+
+//onclick events to get position of clicked tile, blank tile, update them and unlock the corresponding ones
+tiles.map(tile => {
+  tile.addEventListener("click", event => {
+    const tileArea = tile.style.getPropertyValue("--char");
+    const emptyTileArea = emptyTile.style.getPropertyValue("--char");
+    emptyTile.style.setProperty("--char", tileArea);
+    tile.style.setProperty("--char", emptyTileArea);
+    unlockTiles(tileArea);
+  });
+});
+
+//get current positions and compare with the solved puzzle positions
+const isComplete = tiles => {
+  const currentTilesString = tiles
+    .map(tile => tile.style.getPropertyValue("--char"))
+    .toString();
+  if (currentTilesString == Object.keys(charInteractions).toString()) {
+    footer.innerHTML = "YOU WIN!";
+  }
+};
+
+//check if the current shuffle is solvable, logic from https://www.geeksforgeeks.org/check-instance-15-puzzle-solvable/
+const inversions = array => {
+  return array.reduce((accumulator, current, index, array) => {
+    return array
+      .slice(index)
+      .filter(item => {
+        return item < current;
+      })
+      .map(item => {
+        return [current, item];
+      })
+      .concat(accumulator);
+  }, []).length;
+};
+
+const shuffler = keys => Object.keys(keys).sort(() => .5 - Math.random());
+
+//start the game, show tiles in order, shuffle and check if solvable after 2.5 seconds and lock/unlock tiles
+setTimeout(() => {
+  let startingAreas = Object.keys(charInteractions);
+  while (inversions(startingAreas) % 2 == 1 || inversions(startingAreas) == 0) {
+    startingAreas = shuffler(charInteractions);
+  }
+  tiles.map((tile, index) => {
+    tile.style.setProperty("--char", startingAreas[index]);
+  });
+  unlockTiles(emptyTile.style.getPropertyValue("--char"));
+}, 2500);
